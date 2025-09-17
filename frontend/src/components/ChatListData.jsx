032642503c628761365fb15cc0571532f1729256
CJ Yetman <cj@cjyetman.com>
2025-09-17 10:20:42 +0200

    add ID and Web URL to CHat List table

--- frontend/src/components/ChatListData.jsx
+++ frontend/src/components/ChatListData.jsx
@@ -10,11 +10,15 @@ export const ChatListData = (props) => {
             <table>
                 <thead><tr>
                     <td style={{borderWidth: 0.5}}>Topic</td>
+                    <td style={{borderWidth: 0.5}}>ID</td>
+                    <td style={{borderWidth: 0.5}}>Web URL</td>
                 </tr></thead>
                 <tbody>
                     {props.graphData.value.map((data, index) => (
                         <tr key={index}>
                             <td style={{borderWidth: 0.5}}>{data.topic}</td>
+                            <td style={{borderWidth: 0.5}}>{data.id}</td>
+                            <td style={{borderWidth: 0.5}}>{data.webUrl}</td>
                         </tr>
                     ))}
                 </tbody>
