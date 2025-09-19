fa554f1444961a62ff22d21e044b2ce0d1dd1c7d
CJ Yetman <cj@cjyetman.com>
2025-09-19 09:54:11 +0200

    cleam up

--- frontend/src/components/FilesData.jsx
+++ frontend/src/components/FilesData.jsx
@@ -8,35 +8,52 @@ export const FilesData = (props) => {
     const data = props.graphData.value;
     const urls = data.map(d => d["@microsoft.graph.downloadUrl"]);
 
-    console.log(urls);
-
-/*
-    async function get(url) {
-        const config: OfficeParserConfig = {
+    async function get_content(url) {
+        const config = {
             newlineDelimiter: " ",  // Separate new lines with a space instead of the default \n.
             ignoreNotes: true       // Ignore notes while parsing presentation files like pptx or odp.
         }
-        console.log(config);
-
         const response = await fetch(url);
         const arrayBuffer = await response.arrayBuffer();
-        console.log(response);
-        console.log(arrayBuffer);
-        //const result = await officeParser.parseOfficeAsync(arrayBuffer, config);
-        //console.log(result);
-        //console.log("\n\n\n\n\n\n");
-        //return result;
+        const result = await officeParser.parseOfficeAsync(arrayBuffer, config);
+        console.log(await result);
+        return result;
     }
 
-    const text = get(urls[0]);
-    console.log(text);
-    console.log("\n\n\n\n\n\n");
-*/
+    window.urls = urls;
+    console.log("window.urls = urls;");
+    window.get_content = get_content;
+
+    async function get_all_files_content(urls) {
+        const result = await urls.map(async (url) => {
+            let result;
+            get_content(url)
+                .then((response) => {
+                    result = {
+                        id: "e.id",
+                        type: "file content",
+                        date_time: "e.lastModifiedDateTime",
+                        author: "e.from.user.displayName",
+                        content: response || "",
+                        subject: "e.subject"
+                    };
+                });
+            console.log(await result);
+            return await result;
+        })
+        return await result;
+    }
+
+    let file_content;
+    get_all_files_content(urls).then((res) => file_content = res);
+
+    console.log(file_content);
+    window.file_content = file_content;
 
     return (
         <div id="files-div">
             <pre style={{textAlign: "left"}}>
-                {JSON.stringify(text, null, 2) }
+                {JSON.stringify(file_content, null, 2) }
             </pre>
         </div>
     );
