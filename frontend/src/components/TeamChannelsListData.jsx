c27eb5118de2919364bd27f2bdeb3d4f9493fc8c
CJ Yetman <cj@cjyetman.com>
2025-09-23 23:51:41 +0200

    enable selecting channels from the list

--- frontend/src/components/TeamChannelsListData.jsx
+++ frontend/src/components/TeamChannelsListData.jsx
@@ -5,41 +5,29 @@ import React from "react";
  * @param props
  */
 export const TeamChannelsListData = (props) => {
+    const data = props.graphData.flat().filter(n => n);
+
     return (
         <div id="chatslist-div">
             <table>
                 <thead><tr>
                     <td style={{borderWidth: 0.5}}>select</td>
                     <td style={{borderWidth: 0.5}}>Channel Name</td>
-                    <td style={{borderWidth: 0.5}}>Channel ID</td>
                     <td style={{borderWidth: 0.5}}>Team</td>
                     <td style={{borderWidth: 0.5}}>Team Description</td>
-                    <td style={{borderWidth: 0.5}}>Team ID</td>
                     <td style={{borderWidth: 0.5}}>Web URL</td>
                 </tr></thead>
                 <tbody>
-                    {props.graphData.filter(n => n).map((data, index) => (
+                    {data.map((data, index) => (
                         <tr key={index}>
                             <td style={{borderWidth: 0.5}}>
-                                <input type="radio" id={data.id} name="teamchannel_id" value={data.id} />
+                                <input type="radio" name="teamchannel_id" data-team_id={data.team_id} data-channel_id={data.id} />
                             </td>
-                            <td style={{borderWidth: 0.5}}>{data[0].displayName}</td>
+                            <td style={{borderWidth: 0.5}}>{data.displayName}</td>
+                            <td style={{borderWidth: 0.5}}>{data.team_name}</td>
+                            <td style={{borderWidth: 0.5}}>{data.team_desc}</td>
                             <td style={{borderWidth: 0.5}}>
-                                {data[0].id}&nbsp;
-                                <button type="button" class="btn btn-primary" onClick={()=>navigator.clipboard.writeText(data[0].id)}>
-                                  <i class="bi bi-clipboard"></i>
-                                </button>
-                            </td>
-                            <td style={{borderWidth: 0.5}}>{data[0].team_name}</td>
-                            <td style={{borderWidth: 0.5}}>{data[0].team_desc}</td>
-                            <td style={{borderWidth: 0.5}}>
-                                {data[0].team_id}&nbsp;
-                                <button type="button" class="btn btn-primary" onClick={()=>navigator.clipboard.writeText(data[0].team_id)}>
-                                  <i class="bi bi-clipboard"></i>
-                                </button>
-                            </td>
-                            <td style={{borderWidth: 0.5}}>
-                                <a href={data[0].webUrl} target="blank">web URL</a>
+                                <a href={data.webUrl} target="blank">web URL</a>
                             </td>
                         </tr>
                     ))}
