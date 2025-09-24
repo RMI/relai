743bdc0c2b762a0223acec9ce2347b889cf58e32
CJ Yetman <cj@cjyetman.com>
2025-09-24 10:53:29 +0200

    use react-bootstrap tables

--- frontend/src/components/ChatListData.jsx
+++ frontend/src/components/ChatListData.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Table from 'react-bootstrap/Table';
 
 /**
  * Renders information about the user obtained from MS Graph
@@ -7,33 +8,33 @@ import React from "react";
 export const ChatListData = (props) => {
     return (
         <div id="chatslist-div">
-            <table>
+            <Table striped bordered hover size="sm">
                 <thead><tr>
-                    <td style={{borderWidth: 0.5}}>select</td>
-                    <td style={{borderWidth: 0.5}}>Topic</td>
-                    <td style={{borderWidth: 0.5}}>ID</td>
-                    <td style={{borderWidth: 0.5}}>Web URL</td>
+                    <th>select</th>
+                    <th>Topic</th>
+                    <th>ID</th>
+                    <th>Web URL</th>
                 </tr></thead>
                 <tbody>
                     {props.graphData.value.map((data, index) => (
                         <tr key={index}>
-                            <td style={{borderWidth: 0.5}}>
+                            <td>
                                 <input type="radio" id={data.id} name="chat_id" value={data.id} />
                             </td>
-                            <td style={{borderWidth: 0.5}}>{data.topic}</td>
-                            <td style={{borderWidth: 0.5}}>
+                            <td>{data.topic}</td>
+                            <td>
                                 {data.id}&nbsp;
                                 <button type="button" class="btn btn-primary" onClick={()=>navigator.clipboard.writeText(data.id)}>
                                   <i class="bi bi-clipboard"></i>
                                 </button>
                             </td>
-                            <td style={{borderWidth: 0.5}}>
+                            <td>
                                 <a href={data.webUrl} target="blank">web URL</a>
                             </td>
                         </tr>
                     ))}
                 </tbody>
-            </table>
+            </Table>
         </div>
     );
 };
--- frontend/src/components/TeamChannelsListData.jsx
+++ frontend/src/components/TeamChannelsListData.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Table from 'react-bootstrap/Table';
 
 /**
  * Renders information about the user obtained from MS Graph
@@ -7,44 +8,44 @@ import React from "react";
 export const TeamChannelsListData = (props) => {
     return (
         <div id="chatslist-div">
-            <table>
+            <Table striped bordered hover size="sm">
                 <thead><tr>
-                    <td style={{borderWidth: 0.5}}>select</td>
-                    <td style={{borderWidth: 0.5}}>Channel Name</td>
-                    <td style={{borderWidth: 0.5}}>Channel ID</td>
-                    <td style={{borderWidth: 0.5}}>Team</td>
-                    <td style={{borderWidth: 0.5}}>Team Description</td>
-                    <td style={{borderWidth: 0.5}}>Team ID</td>
-                    <td style={{borderWidth: 0.5}}>Web URL</td>
+                    <th>select</th>
+                    <th>Channel Name</th>
+                    <th>Channel ID</th>
+                    <th>Team</th>
+                    <th>Team Description</th>
+                    <th>Team ID</th>
+                    <th>Web URL</th>
                 </tr></thead>
                 <tbody>
                     {props.graphData.filter(n => n).map((data, index) => (
                         <tr key={index}>
-                            <td style={{borderWidth: 0.5}}>
+                            <td>
                                 <input type="radio" id={data.id} name="teamchannel_id" value={data.id} />
                             </td>
-                            <td style={{borderWidth: 0.5}}>{data[0].displayName}</td>
-                            <td style={{borderWidth: 0.5}}>
+                            <td>{data[0].displayName}</td>
+                            <td>
                                 {data[0].id}&nbsp;
                                 <button type="button" class="btn btn-primary" onClick={()=>navigator.clipboard.writeText(data[0].id)}>
                                   <i class="bi bi-clipboard"></i>
                                 </button>
                             </td>
-                            <td style={{borderWidth: 0.5}}>{data[0].team_name}</td>
-                            <td style={{borderWidth: 0.5}}>{data[0].team_desc}</td>
-                            <td style={{borderWidth: 0.5}}>
+                            <td>{data[0].team_name}</td>
+                            <td>{data[0].team_desc}</td>
+                            <td>
                                 {data[0].team_id}&nbsp;
                                 <button type="button" class="btn btn-primary" onClick={()=>navigator.clipboard.writeText(data[0].team_id)}>
                                   <i class="bi bi-clipboard"></i>
                                 </button>
                             </td>
-                            <td style={{borderWidth: 0.5}}>
+                            <td>
                                 <a href={data[0].webUrl} target="blank">web URL</a>
                             </td>
                         </tr>
                     ))}
                 </tbody>
-            </table>
+            </Table>
         </div>
     );
 };
