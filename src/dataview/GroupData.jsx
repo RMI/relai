db5dc985817ef8dbca75fc806fb852f894c7ef07
CJ Yetman <cj@cjyetman.com>
2025-10-01 00:50:44 +0200

    simplify GroupData

--- src/dataview/GroupData.jsx
+++ src/dataview/GroupData.jsx
@@ -5,15 +5,11 @@ import React from "react";
  */
 export const GroupData = (props) => {
     const data = props.graphData.value;
-    const result = data.map(e => ({
-        id: e.id,
-        tenantId: e.tenantId,
-        displayName: e.displayName,
-    }));
+
     return (
         <div id="api-div">
             <pre style={{textAlign: "left"}}>
-                {JSON.stringify(result, null, 2) }
+                {JSON.stringify(data, null, 2) }
             </pre>
         </div>
     );
