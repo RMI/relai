40ecea22321482d57ce3dd4b1a15cfea57c49db2
CJ Yetman <cj@cjyetman.com>
2025-10-11 14:30:12 +0200

    wrap together as an InputGroup

--- src/components/SharePointDropdown.jsx
+++ src/components/SharePointDropdown.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import Form from 'react-bootstrap/Form';
 import { useMsal } from '@azure/msal-react';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 import { loginRequest } from '../authConfig';
 import { getGraphResponse } from '../graph';
@@ -33,24 +34,25 @@ export const SharePointDropdown = () => {
     return (
         <div
             name = 'sharepoint_files_select'
-            style = {{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
         >
-            <Form.Select
-                aria-label = "Select SharePoint Site"
-                style = {{ maxWidth: '250px' }}
-                name = "sharepoint"
-            >
-                <option>Select SharePoint Site</option>
-                {graphData &&
-                    <SharePointDropdownData graphData={graphData} />
-                }
-            </Form.Select>
-            <Form.Control
-                type = "text"
-                placeholder = {path_placeholder}
-                style = {{ maxWidth: '400px' }}
-                name = "sharepoint_path"
-            />
+            <InputGroup className='container-md justify-content-center'>
+                <Form.Select
+                    aria-label = "Select SharePoint Site"
+                    style = {{ maxWidth: '250px' }}
+                    name = "sharepoint"
+                >
+                    <option>Select SharePoint Site</option>
+                    {graphData &&
+                        <SharePointDropdownData graphData={graphData} />
+                    }
+                </Form.Select>
+                <Form.Control
+                    type = "text"
+                    placeholder = {path_placeholder}
+                    style = {{ maxWidth: '400px' }}
+                    name = "sharepoint_path"
+                />
+            </InputGroup>
         </div>
     );
 };
