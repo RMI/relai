23c8cb40cad490f76f3b7245d11cbe59eb1e4e09
CJ Yetman <cj@cjyetman.com>
2025-10-13 16:07:37 +0200

    remove deprecated utilities (#90)

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,16 +1,8 @@
 import { APIContent } from './APIContent';
-import { ChannelMessageListContent } from './ChannelMessageListContent';
 import { Channels } from './Channels';
 import { ChannelsDropdown } from './ChannelsDropdown';
-import { ChatMessagesContent } from './ChatMessagesContent';
 import { Chats } from './Chats';
 import { ChatsDropdown } from './ChatsDropdown';
-import { EmailContent } from './EmailContent';
-import { FilesContent } from './FilesContent';
-import { FilesList } from './FilesList';
-import { GroupsContent } from './GroupsContent';
-import { GroupFilesContent } from './GroupFilesContent';
-import { GroupFilesList } from './GroupFilesList';
 import { PageLayout } from './PageLayout';
 import { ProfileContent } from './ProfileContent';
 import { SharePointDropdown } from './SharePointDropdown';
@@ -23,18 +15,10 @@ import { TeamsFiles } from './TeamsFiles';
 
 export {
     APIContent,
-    ChannelMessageListContent,
     Channels,
     ChannelsDropdown,
-    ChatMessagesContent,
     Chats,
     ChatsDropdown,
-    EmailContent,
-    FilesContent,
-    FilesList,
-    GroupsContent,
-    GroupFilesContent,
-    GroupFilesList,
     PageLayout,
     ProfileContent,
     SharePointDropdown,
